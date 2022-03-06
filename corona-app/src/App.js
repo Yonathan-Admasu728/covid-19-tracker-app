0f284fab15f2325230e7422adc752567573404ae
Yonathan  Admasu <75287408+Yonathan-Admasu728@users.noreply.github.com>
2022-03-05 18:31:45 -0800

    initialized the project by installing react-chartjs-2, react-countup, axios and all other dependencies. also fleshed out the components structure

---
+++ corona-app/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+
+
+class  App extends React.Component {
+  render(){
+    return (
+      <div>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+
+  }
+ 
+}
+
+export default App;
---
+++ corona-app/src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CountryPicker() {
+  return (
+    <div>CountryPicker</div>
+  )
+}
